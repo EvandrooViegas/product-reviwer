750be2bf03506c9c5f85cc1c2380cc1086682bc8
Evandro Viegas <evandrooviegas@gmail.com>
2023-05-25 22:47:48 +0100

    added env variables

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@sanity/client"
 
-const projectId = import.meta.env.VITE_SANITY_PROJECT_ID || "ofomfxmw"
-const dataset = import.meta.env.VITE_SANITY_DATASET || "production"
-const token = import.meta.env.VITE_SANITY_TOKEN || ""
+const projectId = import.meta.env.VITE_SANITY_PROJECT_ID 
+const dataset = import.meta.env.VITE_SANITY_DATASET 
+const token = import.meta.env.VITE_SANITY_TOKEN 
 export const sanity = createClient({
     projectId,
     dataset,
